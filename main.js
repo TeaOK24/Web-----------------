2872bbf4be3d5f787f549c2736c1a3c0f3c12d49
Kirill Babino <kbabino@bk.ru>
2022-04-28 19:34:03 +0300

    доделал карту

---
+++ main.js
@@ -0,0 +1,120 @@
+/*США*/
+let elem1 = document.querySelector('.USA')
+let text1 = document.querySelector('.p1')
+
+
+
+
+elem1.addEventListener('mouseover', function () {
+    text1.classList.add("light");
+})
+
+elem1.addEventListener('mouseleave', function () {
+    text1.classList.remove("light");
+})
+
+
+/*Мексика*/
+let elem2 = document.querySelector('.Mexica')
+let text2 = document.querySelector('.p2')
+
+
+
+
+elem2.addEventListener('mouseover', function () {
+    text2.classList.add("light");
+})
+
+elem2.addEventListener('mouseleave', function () {
+    text2.classList.remove("light");
+})
+
+/*Европа*/
+let elem3 = document.querySelector('.Europa')
+let text3 = document.querySelector('.p3')
+
+
+
+
+elem3.addEventListener('mouseover', function () {
+    text3.classList.add("light");
+})
+
+elem3.addEventListener('mouseleave', function () {
+    text3.classList.remove("light");
+})
+
+/*Африка*/
+let elem4 = document.querySelector('.Africa')
+let text4 = document.querySelector('.p4')
+
+
+
+
+elem4.addEventListener('mouseover', function () {
+    text4.classList.add("light");
+})
+
+elem4.addEventListener('mouseleave', function () {
+    text4.classList.remove("light");
+})  
+
+/*Россия*/
+let elem5 = document.querySelector('.Russia')
+let text5 = document.querySelector('.p5')
+
+
+
+
+elem5.addEventListener('mouseover', function () {
+    text5.classList.add("light");
+})
+
+elem5.addEventListener('mouseleave', function () {
+    text5.classList.remove("light");
+})  
+
+/*Китай*/
+let elem6 = document.querySelector('.China')
+let text6 = document.querySelector('.p6')
+
+
+
+
+elem6.addEventListener('mouseover', function () {
+    text6.classList.add("light");
+})
+
+elem6.addEventListener('mouseleave', function () {
+    text6.classList.remove("light");
+})  
+
+/*Корея*/
+let elem7 = document.querySelector('.Korea')
+let text7 = document.querySelector('.p7')
+
+
+
+
+elem7.addEventListener('mouseover', function () {
+    text7.classList.add("light");
+})
+
+elem7.addEventListener('mouseleave', function () {
+    text7.classList.remove("light");
+})  
+
+/*Япония*/
+let elem8 = document.querySelector('.Japanese')
+let text8 = document.querySelector('.p8')
+
+
+
+
+elem8.addEventListener('mouseover', function () {
+    text8.classList.add("light");
+})
+
+elem8.addEventListener('mouseleave', function () {
+    text8.classList.remove("light");
+})  
