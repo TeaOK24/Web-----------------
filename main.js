ac8076af9dfc8d1a55b79b145ef5a77672f74f2f
Kirill Babino <kbabino@bk.ru>
2022-05-04 23:26:48 +0300

    поместил видео, переделал меню

--- main.js
+++ main.js
@@ -1,120 +1,121 @@
 /*США*/
-let elem1 = document.querySelector('.USA')
-let text1 = document.querySelector('.textdown_a1')
+let elem1 = document.querySelector('.window_map-USA')
+let text1 = document.querySelector('.window_map-a1')
 
 
 
 
 elem1.addEventListener('mouseover', function () {
-    text1.classList.add("light");
+    text1.classList.add("window_map-light");
 })
 
 elem1.addEventListener('mouseleave', function () {
-    text1.classList.remove("light");
+    text1.classList.remove("window_map-light");
 })
 
 
 /*Мексика*/
-let elem2 = document.querySelector('.Mexica')
-let text2 = document.querySelector('.textdown_a2')
+let elem2 = document.querySelector('.window_map-Mexica')
+let text2 = document.querySelector('.window_map-a2')
 
 
 
 
 elem2.addEventListener('mouseover', function () {
-    text2.classList.add("light");
+    text2.classList.add("window_map-light");
 })
 
 elem2.addEventListener('mouseleave', function () {
-    text2.classList.remove("light");
+    text2.classList.remove("window_map-light");
 })
 
 /*Европа*/
-let elem3 = document.querySelector('.Europa')
-let text3 = document.querySelector('.textdown_a3')
+let elem3 = document.querySelector('.window_map-Europa')
+let text3 = document.querySelector('.window_map-a3')
 
 
 
 
 elem3.addEventListener('mouseover', function () {
-    text3.classList.add("light");
+    text3.classList.add("window_map-light");
 })
 
 elem3.addEventListener('mouseleave', function () {
-    text3.classList.remove("light");
+    text3.classList.remove("window_map-light");
 })
 
 /*Африка*/
-let elem4 = document.querySelector('.Africa')
-let text4 = document.querySelector('.textdown_a4')
+let elem4 = document.querySelector('.window_map-Africa')
+let text4 = document.querySelector('.window_map-a4')
 
 
 
 
 elem4.addEventListener('mouseover', function () {
-    text4.classList.add("light");
+    text4.classList.add("window_map-light");
 })
 
 elem4.addEventListener('mouseleave', function () {
-    text4.classList.remove("light");
+    text4.classList.remove("window_map-light");
 })  
 
 /*Россия*/
-let elem5 = document.querySelector('.Russia')
-let text5 = document.querySelector('.textdown_a5')
+let elem5 = document.querySelector('.window_map-Russia')
+let text5 = document.querySelector('.window_map-a5')
 
 
 
 
 elem5.addEventListener('mouseover', function () {
-    text5.classList.add("light");
+    text5.classList.add("window_map-light");
 })
 
 elem5.addEventListener('mouseleave', function () {
-    text5.classList.remove("light");
+    text5.classList.remove("window_map-light");
 })  
 
 /*Китай*/
-let elem6 = document.querySelector('.China')
-let text6 = document.querySelector('.textdown_a6')
+let elem6 = document.querySelector('.window_map-China')
+let text6 = document.querySelector('.window_map-a6')
 
 
 
 
 elem6.addEventListener('mouseover', function () {
-    text6.classList.add("light");
+    text6.classList.add("window_map-light");
 })
 
 elem6.addEventListener('mouseleave', function () {
-    text6.classList.remove("light");
+    text6.classList.remove("window_map-light");
 })  
 
 /*Корея*/
-let elem7 = document.querySelector('.Korea')
-let text7 = document.querySelector('.textdown_a7')
+let elem7 = document.querySelector('.window_map-Korea')
+let text7 = document.querySelector('.window_map-a7')
 
 
 
 
 elem7.addEventListener('mouseover', function () {
-    text7.classList.add("light");
+    text7.classList.add("window_map-light");
 })
 
 elem7.addEventListener('mouseleave', function () {
-    text7.classList.remove("light");
+    text7.classList.remove("window_map-light");
 })  
 
 /*Япония*/
-let elem8 = document.querySelector('.Japanese')
-let text8 = document.querySelector('.textdown_a8')
+let elem8 = document.querySelector('.window_map-Japanese')
+let text8 = document.querySelector('.window_map-a8')
 
 
 
 
 elem8.addEventListener('mouseover', function () {
-    text8.classList.add("light");
+    text8.classList.add("window_map-light");
 })
 
 elem8.addEventListener('mouseleave', function () {
-    text8.classList.remove("light");
+    text8.classList.remove("window_map-light");
 })  
+
